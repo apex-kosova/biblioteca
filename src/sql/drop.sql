08c77b3d4909db38ebe1d5453d12e635d2347bb4
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2019-06-22 22:09:08 -0500

    drop tablas y secuencias.

---
+++ src/sql/drop.sql
@@ -0,0 +1,76 @@
+drop TABLE "AUTOR"
+cascade constraints
+/
+
+drop TABLE "CATEGORIA"
+cascade constraints
+/
+
+drop TABLE "EDITORIAL"
+cascade constraints
+/
+
+drop TABLE "LIBRO"
+cascade constraints
+/
+
+drop TABLE "ESCRIBE"
+cascade constraints
+/
+
+drop TABLE "EJEMPLAR"
+cascade constraints
+/
+
+drop TABLE "ALUMNO"
+cascade constraints
+/
+
+drop TABLE "PRESTAMO"
+cascade constraints
+/
+
+drop TABLE "SANCION"
+cascade constraints
+/
+
+drop TABLE "GRUPO"
+cascade constraints
+/
+
+drop TABLE "USUARIO"
+cascade constraints
+/
+
+drop SEQUENCE "ALUMNO_ID_SEQ"
+/
+
+drop SEQUENCE "AUTOR_ID_SEQ"
+/
+
+drop SEQUENCE "CATEGORIA_ID_SEQ"
+/
+
+drop SEQUENCE "EDITORIAL_ID_SEQ"
+/
+
+drop SEQUENCE "EJEMPLAR_ID_SEQ"
+/
+
+drop SEQUENCE "ESCRIBE_ID_SEQ"
+/
+
+drop SEQUENCE "GRUPO_ID_SEQ"
+/
+
+drop SEQUENCE "LIBRO_ID_SEQ"
+/
+
+drop SEQUENCE "PRESTAMO_ID_SEQ"
+/
+
+drop SEQUENCE "SANCION_ID_SEQ"
+/
+
+drop SEQUENCE "USUARIO_ID_SEQ"
+/
